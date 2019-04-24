ef1d4c7e837d9090333e99fe8bf2b0bc0edf0ab8
Vasily93 <markovavasily@gmail.com>
2019-04-24 08:57:32 -0700

    finished tdd at tests

--- test/tests.js
+++ test/tests.js
@@ -8,52 +8,53 @@ function scoreAnswers(whereFromAnswer, planetAnswer, fruitAnswer, tryAnswer, whe
    whereTravelAnswer = whereTravelAnswer.toLowerCase();
 
    let score = 0;
-   if(whereFromAnswer === 'Russia') {
+
+   if(whereFromAnswer === 'russia') {
       score += 1;
    }
-   if(planetAnswer === 'Mars') {
+   if(planetAnswer === 'mars') {
       score += 1;
    }
-   if(fruitAnswer === 'Orange') {
+   if(fruitAnswer === 'orange') {
       score += 1;
    }
-   if(tryAnswer === 'Skydiving') {
+   if(tryAnswer === 'skydiving') {
       score += 1;
    }
-   if(whereTravelAnswer === 'Machu Pikchu') {
+   if(whereTravelAnswer === 'machu pikchu') {
       score += 1;
    }
    return score;
 }
 
+test('all answers are wrong is 0', function(assert) {
+   //Arrange
+   const whereFromAnswer = 'USA';
+   const planetAnswer = 'Moon';
+   const fruitAnswer = 'Apple';
+   const tryAnswer = 'Horseriding';
+   const whereTravelAnswer = 'Australia';
+   const expected = 0;
+
+   //Act
+   const testScore = scoreAnswers(whereFromAnswer, planetAnswer, fruitAnswer, tryAnswer, whereTravelAnswer);
+   
+   //Assert
+   assert.equal(expected, testScore);
+});
+
 test('all asnswers are right is 5', function(assert) {
     //Arrange
-    const whereFromAnswer = 'Russia';
-    const planetAnswer = 'Mars';
-    const fruitAnswer = 'Orange';
-    const tryAnswer = 'Skydiving';
-    const whereTravelAnswer = 'Machu Pikchu';
+    const whereFromAnswer = 'russia';
+    const planetAnswer = 'mars';
+    const fruitAnswer = 'orange';
+    const tryAnswer = 'skydiving';
+    const whereTravelAnswer = 'machu pikchu';
     const expected = 5;
 
     //Act 
-    const score = scoreAnswers(whereFromAnswer, planetAnswer, fruitAnswer, tryAnswer, whereTravelAnswer);
+    const testScore = scoreAnswers(whereFromAnswer, planetAnswer, fruitAnswer, tryAnswer, whereTravelAnswer);
 
     //Assert
-    assert.equal(score, expected);   
-});
-
-test('all answers are wrong is 0', function(assert) {
-    //Arrange
-    const whereFromAnswer = 'USA';
-    const planetAnswer = 'Moon';
-    const fruitAnswer = 'Apple';
-    const tryAnswer = 'Horseriding';
-    const whereTravelAnswer = 'Australia';
-    const expected = 0;
-
-    //Act
-    const score = scoreAnswers(whereFromAnswer, planetAnswer, fruitAnswer, tryAnswer, whereTravelAnswer);
-    
-    //Assert
-    assert.equal(score, expected);
+    assert.equal(expected, testScore);   
 });
