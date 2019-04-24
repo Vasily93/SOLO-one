52a47c9624b3620c1474aba9790b499c3ae65422
Vasily93 <markovavasily@gmail.com>
2019-04-23 19:01:31 -0700

    added test folder

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,59 @@
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+function scoreAnswers(whereFromAnswer, planetAnswer, fruitAnswer, tryAnswer, whereTravelAnswer) {
+   whereFromAnswer = whereFromAnswer.toLowerCase();
+   planetAnswer = planetAnswer.toLowerCase();
+   fruitAnswer = fruitAnswer.toLowerCase();
+   tryAnswer = tryAnswer.toLowerCase();
+   whereTravelAnswer = whereTravelAnswer.toLowerCase();
+
+   let score = 0;
+   if(whereFromAnswer === 'Russia') {
+      score += 1;
+   }
+   if(planetAnswer === 'Mars') {
+      score += 1;
+   }
+   if(fruitAnswer === 'Orange') {
+      score += 1;
+   }
+   if(tryAnswer === 'Skydiving') {
+      score += 1;
+   }
+   if(whereTravelAnswer === 'Machu Pikchu') {
+      score += 1;
+   }
+   return score;
+}
+
+test('all asnswers are right is 5', function(assert) {
     //Arrange
-    // Set up your parameters and expectations
+    const whereFromAnswer = 'Russia';
+    const planetAnswer = 'Mars';
+    const fruitAnswer = 'Orange';
+    const tryAnswer = 'Skydiving';
+    const whereTravelAnswer = 'Machu Pikchu';
+    const expected = 5;
 
     //Act 
-    // Call the function you're testing and set the result to a const
+    const score = scoreAnswers(whereFromAnswer, planetAnswer, fruitAnswer, tryAnswer, whereTravelAnswer);
 
     //Assert
-    assert.equal(true, false);
+    assert.equal(score, expected);   
+});
+
+test('all answers are wrong is 0', function(assert) {
+    //Arrange
+    const whereFromAnswer = 'USA';
+    const planetAnswer = 'Moon';
+    const fruitAnswer = 'Apple';
+    const tryAnswer = 'Horseriding';
+    const whereTravelAnswer = 'Australia';
+    const expected = 0;
+
+    //Act
+    const score = scoreAnswers(whereFromAnswer, planetAnswer, fruitAnswer, tryAnswer, whereTravelAnswer);
+    
+    //Assert
+    assert.equal(score, expected);
 });
