1fbdde489e87f58dd06b113e244cb0606741d272
Vasily93 <markovavasily@gmail.com>
2019-04-26 11:09:42 -0700

    finished the project!!

--- src/app.js
+++ src/app.js
@@ -6,13 +6,21 @@ const fruitInput = document.getElementById('fruit-answer');
 const tryInput = document.getElementById('try-answer');
 const whereTravelInput = document.getElementById('whereTravel-answer');
 const letsTryButton = document.getElementById('letsTry-button');
-
+const quizResult = document.getElementById('quiz-result');
+//
 letsTryButton.addEventListener('click', () => {
    const whereFromAnswer = whereFromInput.value;
    const planetAnswer = planetInput.value;
    const fruitAnswer = fruitInput.value;
    const tryAnswer = tryInput.value;
    const whereTravelAnswer = whereTravelInput.value;
+   //console.log('im here');
+
+   if(whereFromAnswer.trim() === '' || planetAnswer.trim() === '' || fruitAnswer.trim() === '' 
+   || tryAnswer.trim() === '' || whereTravelAnswer.trim() === '') {
+      alert('All anwers a required!');
+      return;
+   }
 
 function scoreAnswers(whereFromAnswer, planetAnswer, fruitAnswer, tryAnswer, whereTravelAnswer) {
    whereFromAnswer = whereFromAnswer.toLowerCase();
@@ -41,12 +49,14 @@ function scoreAnswers(whereFromAnswer, planetAnswer, fruitAnswer, tryAnswer, whe
    return score;
 }
 
+
+
 const score = scoreAnswers(whereFromAnswer, planetAnswer, fruitAnswer, tryAnswer,whereTravelAnswer);
 const message = 'You Answered' + score + '/5 right!';
 
 quizResult.textContent = message;
 
- if(score < 2) {
+ if(score <= 2) {
      quizResult.classList.add('bad');
      quizResult.classList.remove('good');
  }
