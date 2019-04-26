9d784603788945fff73be849901d2d238cf7418e
Vasily93 <markovavasily@gmail.com>
2019-04-26 10:31:10 -0700

    starting working on css 4/25

--- src/app.js
+++ src/app.js
@@ -14,6 +14,44 @@ letsTryButton.addEventListener('click', () => {
    const tryAnswer = tryInput.value;
    const whereTravelAnswer = whereTravelInput.value;
 
-   const score = scoreAnswers(whereFromAnswer, planetAnswer, fruitAnswer, tryAnswer,whereTravelAnswer);
-   const message = 'You Answered' + score + '/5 right!';
+function scoreAnswers(whereFromAnswer, planetAnswer, fruitAnswer, tryAnswer, whereTravelAnswer) {
+   whereFromAnswer = whereFromAnswer.toLowerCase();
+   planetAnswer = planetAnswer.toLowerCase();
+   fruitAnswer = fruitAnswer.toLowerCase();
+   tryAnswer = tryAnswer.toLowerCase();
+   whereTravelAnswer = whereTravelAnswer.toLowerCase();
+
+   let score = 0;
+
+   if(whereFromAnswer === 'russia') {
+      score += 1;
+   }
+   if(planetAnswer === 'mars') {
+      score += 1;
+   }
+   if(fruitAnswer === 'orange') {
+      score += 1;
+   }
+   if(tryAnswer === 'skydiving') {
+      score += 1;
+   }
+   if(whereTravelAnswer === 'machu pikchu') {
+      score += 1;
+   }
+   return score;
+}
+
+const score = scoreAnswers(whereFromAnswer, planetAnswer, fruitAnswer, tryAnswer,whereTravelAnswer);
+const message = 'You Answered' + score + '/5 right!';
+
+quizResult.textContent = message;
+
+ if(score < 2) {
+     quizResult.classList.add('bad');
+     quizResult.classList.remove('good');
+ }
+ else {
+     quizResult.classList.add('good');
+     quizResult.classList.remove('bad');
+ }
 });
